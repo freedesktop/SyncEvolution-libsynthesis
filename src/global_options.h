3ffdf5318d9ca921932da66d602b81239656b7dc
Lukas Zeller <luz@synthesis.ch>
2009-04-22 17:23:09 +0200

    Version changed to 3.2.0.26

--- src/global_options.h
+++ src/global_options.h
@@ -55,8 +55,8 @@
 #endif
 
 #ifndef SYSYNC_BUILDNUMBER
-#define SYSYNC_BUILDNUMBER          25
-#define SYSYNC_BUILDNUMBER_TXT     "25"
+#define SYSYNC_BUILDNUMBER          26
+#define SYSYNC_BUILDNUMBER_TXT     "26"
 #endif
 
 
