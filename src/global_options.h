a0f6c5feadb45dd1a4c7790b720e5a1bd4c47222
Lukas Zeller <luz@synthesis.ch>
2009-06-10 12:30:20 +0200

    engine 3.2.0.31

--- src/global_options.h
+++ src/global_options.h
@@ -55,8 +55,8 @@
 #endif
 
 #ifndef SYSYNC_BUILDNUMBER
-#define SYSYNC_BUILDNUMBER          28
-#define SYSYNC_BUILDNUMBER_TXT     "28"
+#define SYSYNC_BUILDNUMBER          31
+#define SYSYNC_BUILDNUMBER_TXT     "31"
 #endif
 
 
