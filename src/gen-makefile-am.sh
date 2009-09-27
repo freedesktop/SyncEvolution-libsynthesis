9ba430dc2b5eafd6e880d9bb899d1d5e06928bf5
Patrick Ohly <patrick.ohly@intel.com>
2009-09-27 23:03:21 +0200

    autotools: added make rules for server engine (libsynthesisserver.so)
    The selection of files follows the src/*.mk examples. As before,
    the list of files for each library has to be generated before
    calling automake.

--- src/gen-makefile-am.sh
+++ src/gen-makefile-am.sh
@@ -4,11 +4,45 @@
 # automake. This is necessary because automake cannot build a list
 # of source files dynamically.
 
-# find expression for files which are in sysync_SDK/Sources but only
-# need to be compiled into libsynthesissdk.a
-SDK_FILES='-name admindata.cpp -o -name admindata.h -o -name blobs.cpp -o -name blobs.h -o -name dbitem.cpp -o -name dbitem.h -o -name enginemodulebridge.cpp -o -name enginemodulebridge.h -o -name stringutil.cpp -o -name stringutil.h -o -name target_options.h -o -name timeutil.cpp -o -name timeutil.h -o -name UI_util.cpp -o -name UI_util.h'
+# directories which contain sources for the sync engine
+ENGINE_SOURCES="sysync DB_interfaces sysync_SDK/Sources Transport_interfaces/engine platform_adapters"
 
-sed -e "s;@LIBSYNTHESIS_SOURCES@;`find sysync DB_interfaces sysync_SDK/Sources Transport_interfaces/engine platform_adapters syncapps/clientEngine_custom \( -name '*.cpp' -o -name '*.[ch]' \) \! \( ${SDK_FILES} -o -name clientprovisioning_inc.cpp -o -name \*_tables_inc.cpp -o -name syncserver.cpp -o -name syncsessiondispatch.cpp \) -printf '%p '`;" \
+# files needed exclusively for libsynthesissdk.a
+SDK_FILES="-false `sed -e 's/^/-o -name /' <<EOF
+enginemodulebridge.cpp
+enginemodulebridge.h
+stringutil.cpp
+stringutil.h
+target_options.h
+timeutil.cpp
+timeutil.h
+UI_util.cpp
+UI_util.h
+EOF`"
+
+# files needed exclusively for the client engine
+CLIENT_FILES="-false `sed -e 's/^/-o -name /' <<EOF
+binfile.cpp
+binfileimplds.cpp
+binfileimplclient.cpp
+binfilebase.cpp
+engineclientbase.cpp
+syncclient.cpp
+syncclientbase.cpp
+EOF`"
+
+# files needed exclusively for the server engine
+SERVER_FILES="-false `sed -e 's/^/-o -name /' <<EOF
+admindata.cpp
+admindata.h
+blobs.cpp
+blobs.h
+enginesessiondispatch.cpp
+syncserver.cpp
+EOF`"
+
+sed -e "s;@LIBSYNTHESIS_SOURCES@;`find ${ENGINE_SOURCES} syncapps/clientEngine_custom \( -name '*.cpp' -o -name '*.[ch]' \) \! \( ${SDK_FILES} -o ${SERVER_FILES} -o -name clientprovisioning_inc.cpp -o -name \*_tables_inc.cpp -o -name syncsessiondispatch.cpp \) -printf '%p '`;" \
+    -e "s;@LIBSYNTHESISSERVER_SOURCES@;`find ${ENGINE_SOURCES} sysync_SDK/DB_Interfaces/text_db syncapps/serverEngine_custom \( -name '*.cpp' -o -name '*.[ch]' \) \! \( ${SDK_FILES} -o ${CLIENT_FILES} -o -name clientprovisioning_inc.cpp -o -name \*_tables_inc.cpp -o -name syncsessiondispatch.cpp \) -printf '%p '`;" \
     -e "s;@LIBSYNTHESISSDK_SOURCES_BOTH@;`find sysync_SDK/Sources \( -name '*.cpp' -o -name '*.c' \) -printf '%p '`;" \
     -e "s;@LIBSYNTHESISSDK_SOURCES_SDK_ONLY@;`find sysync_SDK/Sources \( -name '*.cpp' -o -name '*.c' \) -a \( ${SDK_FILES} \) -printf '%p '`;" \
     -e "s;@LIBSMLTK_SOURCES@;`find syncml_tk \( -name '*.cpp' -o -name '*.[ch]' \) \! \( -wholename syncml_tk/src/sml/\*/palm/\* -o -wholename syncml_tk/src/sml/\*/win/\* \) -printf '%p '`;" \
