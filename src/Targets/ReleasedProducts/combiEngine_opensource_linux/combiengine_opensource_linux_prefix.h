1d89fe2b09aa7833c591940851fbe76afb8a4825
Lukas Zeller <luz@synthesis.ch>
2009-10-03 15:27:01 +0200

    unilib linux compile: corrected include (no separate combi-engine include file exists here)

--- src/Targets/ReleasedProducts/combiEngine_opensource_linux/combiengine_opensource_linux_prefix.h
+++ src/Targets/ReleasedProducts/combiEngine_opensource_linux/combiengine_opensource_linux_prefix.h
@@ -26,8 +26,8 @@
   #include "target_options.h"
   // - platform specifics
   #include "platform_headers.h"
-  // - precompilable headers
-  #include "combiengine_custom_precomp.h"
+  // - precompilable headers (use those of server, but client is the same)
+  #include "serverengine_custom_precomp.h"
 #else
   // include all headers that are suitable for precompiled
   // C version use
