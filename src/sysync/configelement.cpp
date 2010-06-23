ff38f636f1d7e6d79a5affd2c0b89fba9b151e80
Lukas Zeller <luz@synthesis.ch>
2010-06-23 19:03:22 +0200

    engine: fixed config parsing bug that could cause some elements to get ignored when preceeded by a ifdeffed-out element.
    When in pamo_all (read over everything), we must not issue any expect_all() again, as these mess up the ignore nesting count. So now pamo_all is checked at the very beginning of startElement().

--- src/sysync/configelement.cpp
+++ src/sysync/configelement.cpp
@@ -320,6 +320,11 @@ bool TConfigElement::startElement(const char *aElementName, const char **aAttrib
       fNest,
       aLine
     ));
+    if (fParseMode==pamo_all) {
+      // read over contents, no matter what is inside
+    	fNest++;
+      return true;
+    }
     #ifdef SYSER_REGISTRATION
     // check for locked elements or subtrees
     // - copy parent's lock status
@@ -516,10 +521,12 @@ bool TConfigElement::startElement(const char *aElementName, const char **aAttrib
         return false; // element not known, generate error message
       }
     }
+    /* %%% moved up to beginning
     else if (fParseMode==pamo_all) {
       // read over contents
       fNest++;
     }
+    */
     else {
       ReportError(false,"no XML tag expected here");
     }
@@ -673,7 +680,8 @@ bool TConfigElement::endElement(const char *aElementName, bool aIsDelegated)
           // back to nested
         	fParseMode=pamo_nested;
         }
-        else fParseMode=pamo_element; // back to normal element parsing
+        else
+        	fParseMode=pamo_element; // back to normal element parsing
         return false; // do not exit
       case pamo_all:
       case pamo_nested:
