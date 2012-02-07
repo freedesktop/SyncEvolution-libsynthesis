2dffd37d179a38a565690d15768a51db847eaf83
Patrick Ohly <patrick.ohly@intel.com>
2012-02-07 10:42:30 +0100

    TCustomImplDS::implMarkOnlyUngeneratedForResume: fixed endless loop
    The while() loop gets stuck if any entry in the map has the "delete"
    member set, because then the code does a "continue" which doesn't move
    the pointer beyond that entry.
    
    Fixed by using a for() loop.
    
    Found while experimenting with better 409 conflict handling in the
    binfile client, where one artificial local ID is marked as deleted.

--- src/sysync/customimplds.cpp
+++ src/sysync/customimplds.cpp
@@ -1791,13 +1791,10 @@ void TCustomImplDS::implMarkOnlyUngeneratedForResume(void)
   bool getPrepared = fGetPhasePrepared;
   // now flag all deletes that need resuming
   if (getPhase==gph_deleted) {
-    // if we are still in "deleted" phase, add these first
-    if (!getPrepared)
-      pos=fMapTable.begin();
-    else
-      pos=fDeleteMapPos;
-    // now mark pending deletes
-    while (pos!=fMapTable.end()) {
+    // now mark pending deletes; if we are still in "deleted" phase, add these first
+    for (pos = !getPrepared ? fMapTable.begin() : fDeleteMapPos;
+         pos!=fMapTable.end();
+         ++pos) {
       // check only undeleted map entries
       // Note: non-normal maps are always in deleted state in fMapTable, so these will be skipped as well
       if ((*pos).deleted) continue;
@@ -1806,8 +1803,6 @@ void TCustomImplDS::implMarkOnlyUngeneratedForResume(void)
         // mark this as pending for resume
         (*pos).markforresume=true;
       }
-      // next
-      ++pos;
     }
     getPhase=gph_added_changed;
     getPrepared=false;
