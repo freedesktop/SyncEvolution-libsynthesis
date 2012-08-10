7a63217d7994190a7056bb6f1e5fd6012b0a4e60
Lukas Zeller <luz@plan44.ch>
2012-08-10 22:53:36 +0200

    Engine Version 3.4.0.44

--- src/global_options.h
+++ src/global_options.h
@@ -76,8 +76,8 @@
 #endif
 
 #ifndef SYSYNC_BUILDNUMBER
-#define SYSYNC_BUILDNUMBER          42
-#define SYSYNC_BUILDNUMBER_TXT     "42"
+#define SYSYNC_BUILDNUMBER          44
+#define SYSYNC_BUILDNUMBER_TXT     "44"
 #endif
 
 
