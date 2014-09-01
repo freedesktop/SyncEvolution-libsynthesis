395c06020a004962b1ecc1fefca3f5fd82b71467
Patrick Ohly <patrick.ohly@intel.com>
2014-09-01 11:50:41 +0200

    SMLTK: use sane MemSize_t when stdint.h is available
    A signed long for the memory functions is inconsistent with
    systems that have a standard malloc/realloc. Better use size_t.

--- src/syncml_tk/src/sml/inc/smldef.h
+++ src/syncml_tk/src/sml/inc/smldef.h
@@ -64,6 +64,10 @@
  */
 #include "define.h"
 
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
 
 /*
  * ===================================
@@ -99,7 +103,11 @@ typedef Long_t        Length_t;     /**< System dependent string length */
 typedef Short_t       MemHandle_t;  /**< Memory object Handle  */
 typedef unsigned char *MemPtr_t;    /**< Memory object Pointer */
 typedef void          *VoidPtr_t;   /**< Void Pointer */
+#ifdef HAVE_STDINT_H
+typedef uintptr_t     MemSize_t;    /**< System dependent memory object size */
+#else
 typedef Long_t        MemSize_t;    /**< System dependent memory object size */
+#endif
 typedef unsigned char MemByte_t;    /**< Memory element */
 typedef unsigned int  Flag_t;       /**< A generic flag type. This type is
                                      * used to declare variables in structures
