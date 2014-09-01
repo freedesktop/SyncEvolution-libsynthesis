4c64442edc907a3ed760b7ea33a1aeae874cb5e7
Patrick Ohly <patrick.ohly@intel.com>
2014-09-01 12:29:52 +0200

    SMLTK: call memory functions through pointers
    This additional indirection allows users of the library to replace the
    default implementation from libc.
    
    The feature is optional. In libsynthesis it can be enabled in
    target_options.h. Users of libsynthesis do not have access to
    libmem.h, so they need to provide their own declaration of the
    function pointers.

--- src/syncml_tk/src/sml/inc/smldef.h
+++ src/syncml_tk/src/sml/inc/smldef.h
@@ -99,13 +99,14 @@ typedef Byte_t        Boolean_t;    /**< a boolean */
 
 
 typedef Short_t       Ret_t;        /**< Return Type of API Commands */
-typedef Long_t        Length_t;     /**< System dependent string length */
 typedef Short_t       MemHandle_t;  /**< Memory object Handle  */
 typedef unsigned char *MemPtr_t;    /**< Memory object Pointer */
 typedef void          *VoidPtr_t;   /**< Void Pointer */
 #ifdef HAVE_STDINT_H
+typedef uintptr_t     Length_t;     /**< System dependent string length */
 typedef uintptr_t     MemSize_t;    /**< System dependent memory object size */
 #else
+typedef Long_t        Length_t;     /**< System dependent string length */
 typedef Long_t        MemSize_t;    /**< System dependent memory object size */
 #endif
 typedef unsigned char MemByte_t;    /**< Memory element */
